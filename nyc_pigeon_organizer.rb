3426a492c62513e93f7954f346123e2e9c6a0640
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-05 17:16:55 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,10 +1,13 @@
 def guessing_totally_based_on_desired_output(data)
   new_hash = {}
-  data.each do |atribute, what|
+  data.each do |attribute, what|
     what.each do |what, who|
       who.each do |who|
         new_hash[who] ||= {}
-        new_hash[who][atribute] ||= []
-        new_hash[who][atribute
+        new_hash[who][attribute] ||= []
+        new_hash[who][attribute] << what.to_s
+      end
+  end
+  new_hash
 end  
   
