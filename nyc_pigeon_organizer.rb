453825113d351e6d70c5e3546a3afe82695571fe
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-05 17:23:06 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
-end
+  new_hash = {}
+  data.each do |characteristic, specifics|
+    specifics.each do |specifics, bird|
+      bird.each do |bird|
+        new_hash[bird] ||= {}
+        new_hash[bird][characteristic] ||= []
+        new_hash[bird][characteristic] << specifics.to_s
+      end
+    end
+  end
+    new_hash
+end 
